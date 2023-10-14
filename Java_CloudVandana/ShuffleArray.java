c11188b94870e54064e6de6aacde19cca908ff2e
Vempallivandana <147935239+Vempallivandana@users.noreply.github.com>
2023-10-14 21:37:14 +0530

    Add files via upload

---
+++ Java_CloudVandana/ShuffleArray.java
@@ -0,0 +1,19 @@
+public class ShuffleArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+               shuffleArray(array);
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+    }
+
+    public static void shuffleArray(int[] array) {
+        int n = array.length;
+        for (int i = n - 1; i > 0; i--) {
+            int j = (int) (Math.random() * (i + 1)); 
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
